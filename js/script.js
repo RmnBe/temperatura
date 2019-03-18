ab615fbb4f8d7279fa365d2d132180f559e78bd4
Roman Bednarz <roman.bednarz@gmail.com>
2019-03-18 20:11:17 +0100

    some changes

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ button.addEventListener('click', function(){
 tempC = window.prompt('Podaj temperaturę w stopniach Celsjusza');
 if (isNaN(tempC)) {output.innerHTML = 'Podaj poprawną liczbę' + '<br><br>' + output.innerHTML}
 else {
-	var tempF = tempC * 1.8 +32
-	output.innerHTML = 'Temperatura '+tempC+' w stopniach Celsjusza to '+tempF+' w stopniach Fahrenheita'}
+	var tempF = Math.round(tempC * 1.8 +32)
+	output.innerHTML = 'Temperatura <font size="5"><b>'+tempC+'</b></font> w stopniach Celsjusza to <font size="5"><b>'+tempF+'</b></font> w stopniach Fahrenheita'}
 	if(tempF < 32){
 		output.innerHTML = output.innerHTML + ' a woda jest zamarznięta.';}
 	else if(tempF > 32 && tempF < 212){
@@ -17,31 +17,28 @@ else {
 	else if(tempF == 212){
 		output.innerHTML = output.innerHTML + ' a woda właśnie zaczyna się gotować.';}
 	else if(tempF == 32){
-		output.innerHTML = output.innerHTML + ' a woda właśnie zaczyna zamarzać';}
+		output.innerHTML = output.innerHTML + ' a woda właśnie zaczyna zamarzać.';}
 	else if(tempF>212){
 		output.innerHTML = output.innerHTML + ' a woda występuje w postaci pary wodnej.';}
 }); 
 
-
-var output2 = document.getElementById('greeter-output2');
-output2.innerHTML = 'Naciśnij przycisk żeby obliczyć temperaturę' + '<br><br>' + output2.innerHTML;
 var button2 = document.getElementById('greeter-button2');
 var tempF2;
 button2.addEventListener('click', function(){
 tempF2 = window.prompt('Podaj temperaturę w stopniach Fahrenheita');
-if (isNaN(tempF2)) {output2.innerHTML = 'Podaj poprawną liczbę' + '<br><br>' + output2.innerHTML}
+if (isNaN(tempF2)) {output.innerHTML = 'Podaj poprawną liczbę' + '<br><br>' + output.innerHTML}
 else {
-	var tempC2 = (5 / 9) * (tempF2 - 32)
-	output2.innerHTML = 'Temperatura '+tempF2+' w stopniach Fahrenheita to '+tempC2+' w stopniach Celsjusza'}
+	var tempC2 = Math.round((5 / 9) * (tempF2 - 32))
+	output.innerHTML = 'Temperatura <font size="5"><b>'+tempF2+'</b></font> w stopniach Fahrenheita to <font size="5"><b>'+tempC2+'</b></font> w stopniach Celsjusza'}
 	if(tempF2 < 32){
-		output2.innerHTML = output2.innerHTML + ' a woda jest zamarznięta.';}
+		output.innerHTML = output.innerHTML + ' a woda jest zamarznięta.';}
 	else if(tempF2 > 32 && tempF2 < 212){
-		output2.innerHTML = output2.innerHTML + ' a woda jest w stanie ciekłym.';}
+		output.innerHTML = output.innerHTML + ' a woda jest w stanie ciekłym.';}
 	else if(tempF2 == 212){
-		output2.innerHTML = output2.innerHTML + ' a woda właśnie zaczyna się gotować.';}
+		output.innerHTML = output.innerHTML + ' a woda właśnie zaczyna się gotować.';}
 	else if(tempF2 == 32){
-		output2.innerHTML = output2.innerHTML + ' a woda właśnie zaczyna zamarzać';}
+		output.innerHTML = output.innerHTML + ' a woda właśnie zaczyna zamarzać.';}
 	else if(tempF2>212){
-		output2.innerHTML = output2.innerHTML + ' a woda występuje w postaci pary wodnej.';}
+		output.innerHTML = output.innerHTML + ' a woda występuje w postaci pary wodnej.';}
 });
 
